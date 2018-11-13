40e47feba88ab79f21b77342dec0f8006e3f2d99
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-13 18:49:45 +0200

    Mutable and immutable variables, tyope inference.

--- src/ws/tilda/anastasia/kotlinbasics/basics/Variables.kt
+++ src/ws/tilda/anastasia/kotlinbasics/basics/Variables.kt
@@ -1,5 +1,17 @@
 package ws.tilda.anastasia.kotlinbasics.basics
 
 fun main(args: Array<String>) {
-    
+
+    /* Creating variable of type Integer. It is not a dynamic type. It is mutable variable declaration.
+     * There is a type inference. Not necessarily we need to declare the type.
+     */
+
+    var streetNumber: Int
+    streetNumber = 10
+    var streetName: String = "High Street" //initialized
+
+    streetName = "Central Lane" //Can be changed, therefore mutable.
+
+    val zip = "E11 P1" //Immutable variable. When declared and initialized cannot be reassigned.
+
 }
