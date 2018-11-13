c638af71d07eeea6c589b2e40fb9acceba4c1830
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-13 20:16:41 +0200

    Import remark

--- src/ws/tilda/anastasia/kotlinbasics/basics/Main.kt
+++ src/ws/tilda/anastasia/kotlinbasics/basics/Main.kt
@@ -1,7 +1,9 @@
 package ws.tilda.anastasia.kotlinbasics.basics
 
-import ws.tilda.anastasia.kotlinbasics.basics.utils.someUtility //automatically imported from the package
+import ws.tilda.anastasia.kotlinbasics.basics.utils.someUtility //function automatically imported from the package
+import ws.tilda.anastasia.kotlinbasics.basics.utils.someUtility as someAdditionalFunction //in case of conflicts to rename the imported function
 
 fun main(args: Array<String>) {
     someUtility("Hello")
+    someAdditionalFunction("Hi")
 }
