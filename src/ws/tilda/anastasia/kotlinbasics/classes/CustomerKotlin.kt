5082fa9b7603806e6b0bbddbfb4b292236db35c6
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-14 12:17:10 +0200

    copy the objects

--- src/ws/tilda/anastasia/kotlinbasics/classes/CustomerKotlin.kt
+++ src/ws/tilda/anastasia/kotlinbasics/classes/CustomerKotlin.kt
@@ -5,8 +5,11 @@ data class CustomerKotlin (var id: Int, var name: String, var email: String)
 fun main(args: Array<String>) {
     val customer1 = CustomerKotlin(1, "Anastasiia", "akarpenko@aalto.fi")
     val customer2 = CustomerKotlin(1, "Anastasiia", "akarpenko@aalto.fi")
+    val customer3 = customer1 //can copy the objects
 
     if(customer1 == customer2) {
         println("They are the same")
     }
+
+    println(customer3.email)
 }
