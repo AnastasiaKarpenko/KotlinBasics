1fa7890629e85f8f3e8a0d5eea870c5e4fb97988
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-17 10:16:37 +0200

    Extended function

--- src/ws/tilda/anastasia/kotlinbasics/functional/ExtensionFunctions.kt
+++ src/ws/tilda/anastasia/kotlinbasics/functional/ExtensionFunctions.kt
@@ -4,6 +4,11 @@ fun String.hello() {
     println("It's me!")
 }
 
+fun String.toTitleCase(): String {
+    return this.split(" ").joinToString(" ") {it.capitalize()}
+}
+
 fun main(args: Array<String>) {
     "Hello".hello()
+    println("hello kotlin".toTitleCase())
 }
